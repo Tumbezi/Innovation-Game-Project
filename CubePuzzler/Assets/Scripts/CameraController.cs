813efa08558f41a2413677424b80dd333af20255
s1m 2h <santeri.hytonen@gmail.com>
2023-11-03 17:36:06 +0200

    Added rotation animation for camera when changing sides

--- CubePuzzler/Assets/Scripts/CameraController.cs
+++ CubePuzzler/Assets/Scripts/CameraController.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject cameraRotator;
+    public float rotSpeed;
 
     public void RotateCamera(Transform side)
     {
-        cameraRotator.transform.rotation = side.rotation;
+        StartCoroutine(RotationAnimation(cameraRotator.transform, side));
+        //cameraRotator.transform.rotation = side.rotation;
+    }
+
+    IEnumerator RotationAnimation(Transform current, Transform target)
+    {
+        Quaternion startRot = current.rotation;
+        Quaternion targetRot = target.rotation;
+        float timeElapsed = 0f;
+        Vector3 start = transform.localScale;
+
+        while (timeElapsed <= 1f)
+        {
+            //Lerp new value for axis and scale gate by it
+            Quaternion newRot = Quaternion.Lerp(startRot, targetRot, timeElapsed);
+            cameraRotator.transform.rotation = newRot;
+            timeElapsed += Time.deltaTime * rotSpeed;
+            yield return null;
+        }
+
+        //Ensure that the scale is correct
+        cameraRotator.transform.rotation = targetRot;
+
     }
 }
