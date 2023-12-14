d20e3b6ed3a07ddc9bc75c5de6b8969e72645e1a
Toni <tonimikok@gmail.com>
2023-12-15 01:32:01 +0200

    fix

--- CubePuzzler/Assets/Scripts/MainMenu.cs
+++ CubePuzzler/Assets/Scripts/MainMenu.cs
@@ -16,6 +16,8 @@ public class MainMenu : MonoBehaviour
 
     private void Awake()
     {
+        Time.timeScale = 1;
+
         for (int i= 0; i < GameManager.Instance.levelUnlockStatus.Count; i++)
         {
             buttons[i].GetComponent<LevelButton>().unlockedStatus = true;
